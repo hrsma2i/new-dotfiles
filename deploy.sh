06053f2dc42cfc3f702bbcae071aa3f3e7e78e65
hrsma2i <hrs.ma2i@gmail.com>
2018-03-18 18:00:25 +0900

    Automate initialize jupyter

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,24 @@
 #
 #--------------------------------------------------------------------
 
+function has(){
+	# check whether the command exists
+	type "$1" > /dev/null 2>&1
+	return $?
+}
+
 DOTROOT=$(cd $(dirname $0);pwd)
 
 ln -fns $DOTROOT/.bashrc $HOME/.bashrc
 ln -fns $DOTROOT/nvim/init.vim $HOME/.vimrc
 ln -fns $DOTROOT/nvim $HOME/.vim
-cp -r $DOTROOT/.jupyter $HOME/
+
+# for jupyter
+if has jupyter; then
+	echo "jupyer initialized"
+	cp -r $DOTROOT/.jupyter $HOME/
+	jupyter notebook --generate-config
+	python $DOTROOT/init_jupyter.py
+fi
 
 source $HOME/.bashrc
--- init_jupyter.py
+++ init_jupyter.py
@@ -1,3 +1,9 @@
+#! /usr/bin/env python
+import os
+from notebook.auth import passwd
+from jupyter_core.paths import jupyter_config_dir
+
+content = """
 from jupyter_core.paths import jupyter_config_dir, jupyter_data_dir
 import os.path
 import sys
@@ -5,10 +11,14 @@ import sys
 sys.path.append(os.path.join(jupyter_data_dir(), 'extensions'))
 
 c = get_config()
-
 c.NotebookApp.ip = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888
-c.NotebookApp.password = u'sha1:XXXXXXXXX' 
+c.NotebookApp.password = u'{}'
 c.NotebookApp.extra_template_paths = [os.path.join(jupyter_data_dir(), 'templates') ]
 c.NotebookApp.server_extensions = ['nbextensions']
+""".format(passwd())
+
+config_file = os.path.join(jupyter_config_dir(), "jupyter_notebook_config.py")
+with open(config_file, "a") as f:
+    f.write(content)
