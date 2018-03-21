d2176aab6ab06f82b35c665c4ad05f7f2fa9593b
hrsma2i <hrs.ma2i@gmail.com>
2018-03-21 23:18:29 +0900

    Deploy .tmux.conf

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,5 @@ DOTROOT=$(cd $(dirname $0);pwd)
 ln -fns $DOTROOT/.bashrc $HOME/.bashrc
 ln -fns $DOTROOT/nvim/init.vim $HOME/.vimrc
 ln -fns $DOTROOT/nvim $HOME/.vim
+ln -fns $DOTROOT/.tmux.conf $HOME/.tmux.conf
 ln -fns $DOTROOT/.jupyter $HOME/.jupyter
